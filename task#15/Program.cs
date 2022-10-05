f1b309d454e2c016fbe8ec39746d7db8969308c5
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-05 13:44:07 +0300

    Задача №15_

--- task#15/Program.cs
+++ task#15/Program.cs
@@ -12,37 +12,37 @@ string number = Console.ReadLine();
 if (number == "1")
     {
     Console.WriteLine("Понедельник");
-    Console.WriteLine("1-ый день недеди - БУДНИЙ день");
+    Console.WriteLine("1-ый день недели - БУДНИЙ день");
     }
 else
     Console.WriteLine("Вы ввели цифру не от 1 до 7. Попробуйте еще раз! У Вас обязательно получится!");
 if (number == "2")
     {
         Console.WriteLine("Вторник");
-        Console.WriteLine("2-ой день недеди - БУДНИЙ день");
+        Console.WriteLine("2-ой день недели - БУДНИЙ день");
     }  
 if (number == "3")
     {
         Console.WriteLine("Среда");
-        Console.WriteLine("3-ий день недеди - БУДНИЙ день");
+        Console.WriteLine("3-ий день недели - БУДНИЙ день");
     } 
 if (number == "4")
     {
         Console.WriteLine("Четверг");
-        Console.WriteLine("4-ый день недеди - БУДНИЙ день");
+        Console.WriteLine("4-ый день недели - БУДНИЙ день");
     }
 if (number == "5")
     {
         Console.WriteLine("Пятница");
-        Console.WriteLine("5-ый день недеди - БУДНИЙ день");
+        Console.WriteLine("5-ый день недели - БУДНИЙ день");
     }
 if (number == "6")
     {
         Console.WriteLine("Суббота");
-        Console.WriteLine("6-ой день недеди ВЫХОДНОЙ");
+        Console.WriteLine("6-ой день недели ВЫХОДНОЙ");
     }    
 if (number == "7")
     {
         Console.WriteLine("Воскресенье");
-        Console.WriteLine("7-ой день недеди ВЫХОДНОЙ");
+        Console.WriteLine("7-ой день недели ВЫХОДНОЙ");
     }
