2f9cde7a5603add1af6f17772efc9def982d1988
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-05 23:37:18 +0300

    Задача №15_вариант решения №2_

--- task#15_var.2/Program.cs
+++ task#15_var.2/Program.cs
@@ -1,6 +1,11 @@
 ﻿int number = new Random().Next(1,7);
 Console.WriteLine(number);
 
+/* если с подстановкой чисел то решение будет вот такое:
+int number = 7;
+Console.WriteLine($"{number}");
+*/
+
 if(number >= 1 && number<=5)
 Console.Write("не выходной");
 else if(number == 6 || number == 7)
