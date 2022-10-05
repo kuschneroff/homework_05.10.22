6f26aa60efae96ff02102e6436b7600ec6285c07
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-05 23:25:29 +0300

    Задача №15_вариант решения №2

---
+++ task#15_var.2/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = new Random().Next(1,7);
+Console.WriteLine(number);
+
+if(number >= 1 && number<=5)
+Console.Write("не выходной");
+else if(number == 6 || number == 7)
+Console.Write("выходной");
+else
+Console.Write("ввели не правильный день недели");
