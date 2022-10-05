301007cfd5e3fb58a3441438722e9dfa0086bdd6
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-05 13:40:17 +0300

    Задача №15

---
+++ task#15/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Задача 15: Напишите программу, 
+которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Введите цифру от 1 до 7");
+string number = Console.ReadLine();
+if (number == "1")
+    {
+    Console.WriteLine("Понедельник");
+    Console.WriteLine("1-ый день недеди - БУДНИЙ день");
+    }
+else
+    Console.WriteLine("Вы ввели цифру не от 1 до 7. Попробуйте еще раз! У Вас обязательно получится!");
+if (number == "2")
+    {
+        Console.WriteLine("Вторник");
+        Console.WriteLine("2-ой день недеди - БУДНИЙ день");
+    }  
+if (number == "3")
+    {
+        Console.WriteLine("Среда");
+        Console.WriteLine("3-ий день недеди - БУДНИЙ день");
+    } 
+if (number == "4")
+    {
+        Console.WriteLine("Четверг");
+        Console.WriteLine("4-ый день недеди - БУДНИЙ день");
+    }
+if (number == "5")
+    {
+        Console.WriteLine("Пятница");
+        Console.WriteLine("5-ый день недеди - БУДНИЙ день");
+    }
+if (number == "6")
+    {
+        Console.WriteLine("Суббота");
+        Console.WriteLine("6-ой день недеди ВЫХОДНОЙ");
+    }    
+if (number == "7")
+    {
+        Console.WriteLine("Воскресенье");
+        Console.WriteLine("7-ой день недеди ВЫХОДНОЙ");
+    }
